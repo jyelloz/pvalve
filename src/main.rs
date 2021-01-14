7d0144c045a551dca6036ad6cc383b23d8ce7363
Jordan Yelloz <jordan@yelloz.me>
2021-01-13 20:56:07 -0800

    Added basic pvalve tool.
    This is a "pipe valve" where rate of data flowing from stdin to stdout
    is limited using the existing leaky bucket crate.
    
    While running, you edit the file "control" in the current directory and
    put a number in the first line and it will limit the amount of bytes to
    pass through the filter per second to that value.
    
    The only thing this offers over pv's rate limiting feature is that it
    can change the maximum speed during runtime. It is currently not able to
    operate in "line mode" which is a good feature to add when working with
    line-oriented structured data rather than raw bytes.

---
+++ src/main.rs
@@ -0,0 +1,108 @@
+use std::path::Path;
+
+use tokio::sync::watch::{
+    channel,
+    Sender,
+    Receiver,
+};
+use tokio::io::{
+    self,
+    BufReader,
+    AsyncBufReadExt,
+    AsyncRead,
+    AsyncReadExt,
+    AsyncWrite,
+    AsyncWriteExt,
+};
+use tokio::fs::{
+    File,
+    OpenOptions,
+};
+use tokio::time::sleep;
+use leaky_bucket::LeakyBucket;
+use std::time::Duration;
+
+async fn watch_control_file(path: &str, tx: Sender<usize>) -> io::Result<()> {
+    let path = Path::new(&path);
+    {
+        OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(path)
+            .await?;
+    }
+    loop {
+        let file = File::open(path).await?;
+        let file = BufReader::new(file);
+        let line = file.lines().next_line().await?;
+        let line = line.unwrap_or_default();
+        let rate: Option<usize> = line.parse().ok();
+        match rate {
+            Some(rate) => {
+                tx.send(rate).expect("failed to update rate");
+            },
+            _ => {
+                eprintln!("could not parse control file");
+            },
+        }
+        sleep(Duration::from_secs(1)).await;
+    }
+}
+
+
+fn get_bucket(rate: usize) -> LeakyBucket {
+    LeakyBucket::builder()
+        .max(rate)
+        .tokens(0)
+        .refill_interval(Duration::from_secs(1))
+        .refill_amount(rate)
+        .build()
+        .expect("failed to build leaky bucket")
+}
+
+async fn transfer<I: AsyncRead + Unpin, O: AsyncWrite + Unpin>(
+    mut rx: I,
+    mut tx: O,
+    control: Receiver<usize>,
+) -> io::Result<()> {
+
+    let mut bucket = get_bucket(*control.borrow());
+    let mut buf: [u8; 1024] = [0; 1024];
+    loop {
+
+        let new_rate = *control.borrow();
+        if new_rate != bucket.max() {
+            bucket = get_bucket(new_rate);
+        }
+
+        let bytes_read = rx.read(&mut buf).await?;
+        if bytes_read == 0 {
+            break;
+        }
+
+        tx.write_all(&buf[0..bytes_read]).await?;
+
+        tx.flush().await?;
+
+        bucket.acquire(bytes_read).await.ok();
+
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let (tx, rx) = channel(1024usize);
+    let watch = tokio::spawn(
+        watch_control_file("control", tx)
+    );
+    tokio::spawn(
+        transfer(
+            io::stdin(),
+            io::stdout(),
+            rx,
+        )
+    ).await??;
+    watch.abort();
+    Ok(())
+}
