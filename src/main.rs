0e5666a5162f9ae60b2199332926c06876d8e210
Jordan Yelloz <jordan@yelloz.me>
2021-02-20 20:52:26 -0800

    The syncio has superseded the asyncio version.

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,43 @@
-use std::path::Path;
-
-use tokio::sync::watch::{
-    channel,
-    Sender,
-    Receiver,
-};
-use tokio::io::{
+use std::thread;
+use std::io::{
     self,
-    BufReader,
-    AsyncBufReadExt,
-    AsyncRead,
-    AsyncReadExt,
-    AsyncWrite,
-    AsyncWriteExt,
+    copy,
+    Result,
+    Stdin,
+    Stdout,
 };
-use tokio::fs::{
-    File,
-    OpenOptions,
+use std::sync::mpsc::Sender;
+use std::num::NonZeroU32;
+use nonzero_ext::nonzero;
+
+use pvalve::{
+    syncio::RateLimitedWriter,
+    tui::{
+        user_interface,
+        cleanup,
+    },
 };
-use tokio::time::sleep;
-use leaky_bucket::LeakyBucket;
-use std::time::Duration;
-
-async fn watch_control_file(path: &str, tx: Sender<usize>) -> io::Result<()> {
-    let path = Path::new(&path);
-    {
-        OpenOptions::new()
-            .write(true)
-            .create(true)
-            .open(path)
-            .await?;
-    }
-    loop {
-        let file = File::open(path).await?;
-        let file = BufReader::new(file);
-        let line = file.lines().next_line().await?;
-        let line = line.unwrap_or_default();
-        let rate: Option<usize> = line.parse().ok();
-        match rate {
-            Some(rate) => {
-                tx.send(rate).expect("failed to update rate");
-            },
-            _ => {
-                eprintln!("could not parse control file");
-            },
-        }
-        sleep(Duration::from_secs(1)).await;
-    }
-}
+use crossterm::tty::IsTty;
 
 
-fn get_bucket(rate: usize) -> LeakyBucket {
-    LeakyBucket::builder()
-        .max(rate)
-        .tokens(0)
-        .refill_interval(Duration::from_secs(1))
-        .refill_amount(rate)
-        .build()
-        .expect("failed to build leaky bucket")
-}
-
-async fn transfer<I: AsyncRead + Unpin, O: AsyncWrite + Unpin>(
-    mut rx: I,
-    mut tx: O,
-    buffer_length: usize,
-    control: Receiver<usize>,
-) -> io::Result<()> {
-
-    let mut bucket = get_bucket(*control.borrow());
-    let mut buf = vec![0u8; buffer_length];
-    let buf = &mut buf.as_mut_slice()[0..buffer_length];
-    loop {
-
-        let new_rate = *control.borrow();
-        if new_rate != bucket.max() {
-            bucket = get_bucket(new_rate);
-        }
-
-        let bytes_read = rx.read(buf).await?;
-        if bytes_read == 0 {
-            break;
-        }
-
-        tx.write_all(&buf[0..bytes_read]).await?;
-
-        tx.flush().await?;
-
-        bucket.acquire(bytes_read).await.ok();
-
-    }
-    Ok(())
-}
-
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let buffer_length = 1 << 20;
-    let (tx, rx) = channel(1024usize);
-    let watch = tokio::spawn(
-        watch_control_file("control", tx)
+fn main() -> anyhow::Result<()> {
+    let mut stdin = io::stdin();
+    let stdout = io::stdout();
+    let (tx, rx) = std::sync::mpsc::channel();
+    let ui = if !stdin.is_tty() && !stdout.is_tty() {
+        Some(thread::spawn(move || user_interface(tx)))
+    } else {
+        None
+    };
+    let mut stdout = RateLimitedWriter::writer_with_rate_and_updates(
+        stdout,
+        nonzero!(1u32),
+        rx
     );
-    tokio::spawn(
-        transfer(
-            io::stdin(),
-            io::stdout(),
-            buffer_length,
-            rx,
-        )
-    ).await??;
-    watch.abort();
+    let copy_result = copy(&mut stdin, &mut stdout);
+    if ui.is_some() {
+        cleanup()?;
+    }
+    copy_result?;
     Ok(())
 }
