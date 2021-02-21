e3fa29bf1ac1b64582489c223c7e1b127f2c047c
Jordan Yelloz <jordan@yelloz.me>
2021-02-21 07:46:52 -0800

    main: Formatted some code.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
-use std::thread;
-use std::io::{
-    self,
-    copy,
-};
 use nonzero_ext::nonzero;
+use std::{
+    io::{self, copy},
+    sync::mpsc::channel,
+    thread,
+};
 
+use crossterm::tty::IsTty;
 use pvalve::{
     syncio::RateLimitedWriter,
-    tui::{
-        user_interface,
-        cleanup,
-    },
+    tui::{cleanup, user_interface},
 };
-use crossterm::tty::IsTty;
-
 
 fn main() -> anyhow::Result<()> {
     let mut stdin = io::stdin();
     let stdout = io::stdout();
-    let (tx, rx) = std::sync::mpsc::channel();
+    let (tx, rx) = channel();
     let ui = if !stdin.is_tty() && !stdout.is_tty() {
         Some(thread::spawn(move || user_interface(tx)))
     } else {
@@ -27,7 +23,7 @@ fn main() -> anyhow::Result<()> {
     let mut stdout = RateLimitedWriter::writer_with_rate_and_updates(
         stdout,
         nonzero!(1u32),
-        rx
+        rx,
     );
     let copy_result = copy(&mut stdin, &mut stdout);
     if ui.is_some() {
