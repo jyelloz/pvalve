bd091a5ebd87003ff5a001681eb2b4f7bf276de7
Jordan Yelloz <jordan@yelloz.me>
2021-02-21 13:05:38 -0800

    ipc: Added new stream=>UI message type.
    This can either let the UI figure out how long it's been running for and
    how many bytes have been moved or tell the UI to shut down since the
    copy is done or broken.

--- src/ipc.rs
+++ src/ipc.rs
@@ -1,7 +1,13 @@
-use std::num::NonZeroU32;
+use std::{num::NonZeroU32, time::Duration};
 
 #[derive(Debug, Clone)]
 pub enum Message {
     UpdateRate(NonZeroU32),
+    Interrupted,
+}
+
+#[derive(Debug, Clone)]
+pub enum ProgressMessage {
+    Transfer(usize, Duration),
     Interrupted,
 }
