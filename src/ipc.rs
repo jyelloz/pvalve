ed518fcaa469f8ee6971f9579354f9418d51eb63
Jordan Yelloz <jordan@yelloz.me>
2021-03-05 21:23:22 -0800

    ipc: Removed unused enum variant.

--- src/ipc.rs
+++ src/ipc.rs
@@ -1,4 +1,4 @@
-use std::{num::NonZeroU32, time::Duration};
+use std::num::NonZeroU32;
 
 #[derive(Debug, Clone)]
 pub enum Message {
@@ -9,6 +9,5 @@ pub enum Message {
 #[derive(Debug, Clone)]
 pub enum ProgressMessage {
     Initial,
-    Transfer(usize, Duration),
     Interrupted,
 }
