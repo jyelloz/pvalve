08c9c1f8053fadd3b1ad74bf69e3390b50d69a25
Jordan Yelloz <jordan@yelloz.me>
2021-06-06 10:41:31 -0700

    ipc: Removed unused struct.

--- src/ipc.rs
+++ src/ipc.rs
@@ -1,11 +1,3 @@
-use std::num::NonZeroU32;
-
-#[derive(Debug, Clone)]
-pub enum Message {
-    UpdateRate(NonZeroU32),
-    Interrupted,
-}
-
 #[derive(Debug, Clone)]
 pub enum ProgressMessage {
     Initial,
