40339defc42c271a88db050afd83e7f8b04e6755
Jordan Yelloz <jordan@yelloz.me>
2021-06-06 10:06:01 -0700

    config: Simplified, possibly.
    Relied on the watch crate to have watchable spots in memory that can be
    polled across threads.
    
    The Latch can be used for both cancellation and pausing.

--- src/config.rs
+++ src/config.rs
@@ -1,33 +1,78 @@
-use std::{
-    num::NonZeroU32,
-    sync::{Arc, RwLock},
-};
+use std::num::NonZeroU32;
 
 use nonzero_ext::nonzero;
 
-use lazy_static::lazy_static;
-
-lazy_static! {
-    static ref CURRENT_CONFIG: Arc<RwLock<Config>> = Arc::new(RwLock::new(
-        Default::default()
-    ));
-}
+use watch::{WatchReceiver, WatchSender, channel};
 
 #[derive(Debug, Default, Clone)]
 pub struct Config {
-    pub paused: bool,
     pub limit: Option<NonZeroU32>,
 }
 
+#[derive(Clone)]
+pub struct ConfigMonitor(WatchReceiver<Config>);
+
+pub struct Latch {
+    active: bool,
+    tx: WatchSender<bool>,
+}
+pub struct LatchMonitor(WatchReceiver<bool>);
+
 impl Config {
-    pub fn current() -> Config {
-        CURRENT_CONFIG.read().unwrap().clone()
-    }
-    pub fn make_current(&self) {
-        let mut write = CURRENT_CONFIG.write().unwrap();
-        *write = self.clone();
-    }
     pub fn limit(&self) -> NonZeroU32 {
         self.limit.unwrap_or(nonzero!(1u32))
+    }
+}
+
+impl ConfigMonitor {
+    pub fn new(config: Config) -> (WatchSender<Config>, Self) {
+        let (tx, rx) = channel(config);
+        (tx, Self(rx))
+    }
+    pub fn limit_if_new(&mut self) -> Option<NonZeroU32> {
+        self.0
+            .get_if_new()
+            .map(|config| config.limit())
+    }
+    pub fn limit(&mut self) -> NonZeroU32 {
+        self.0.get().limit()
+    }
+}
+
+impl Latch {
+    pub fn new() -> Self {
+        let active = false;
+        let (tx, _) = channel(active);
+        Self {
+            active,
+            tx,
+        }
+    }
+    pub fn active(&self) -> bool {
+        self.active
+    }
+    pub fn toggle(&mut self) {
+        self.active = !self.active;
+        self.tx();
+    }
+    pub fn on(&mut self) {
+        self.active = true;
+        self.tx();
+    }
+    pub fn off(&mut self) {
+        self.active = false;
+        self.tx();
+    }
+    fn tx(&mut self) {
+        self.tx.send(self.active);
+    }
+    pub fn watch(&mut self) -> LatchMonitor {
+        LatchMonitor(self.tx.subscribe())
+    }
+}
+
+impl LatchMonitor {
+    pub fn active(&mut self) -> bool {
+        self.0.get()
     }
 }
