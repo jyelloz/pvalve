92c92dcbece2dd25c5a3b1850e7f2f89e1f40f44
Jordan Yelloz <jordan@yelloz.me>
2021-06-06 11:03:06 -0700

    Removed unused IPC/memslot.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 pub mod cli;
 pub mod config;
-pub mod ipc;
-pub mod memslot;
 pub mod progress;
 pub mod syncio;
 pub mod tui;
--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,6 @@ use pvalve::{
         Latch,
     },
     cli::Opts,
-    ipc::ProgressMessage,
-    memslot::Memslot,
     syncio::{RateLimitedWriter, WriteExt as _},
     tui::{Cleanup, UserInterface},
 };
@@ -29,20 +27,19 @@ fn main() -> anyhow::Result<()> {
         ..Default::default()
     };
 
-    let (watch_tx, config_watch) = ConfigMonitor::new(config.clone());
+    let (config_tx, config_rx) = ConfigMonitor::new(config.clone());
 
     let stdin = io::stdin();
     let stdout = io::stdout();
-    let (mut state_tx, state_rx) =
-        Memslot::new(ProgressMessage::Initial).split();
 
+    let mut shutdown = Latch::new();
     let mut paused = Latch::new();
     let mut aborted = Latch::new();
 
     let interactive_mode = !stdin.is_tty() && !stdout.is_tty();
     let mut stdout = RateLimitedWriter::writer_with_config(
         stdout,
-        config_watch,
+        config_rx,
     )
         .pauseable(paused.watch())
         .cancellable(aborted.watch())
@@ -51,17 +48,17 @@ fn main() -> anyhow::Result<()> {
         let ui = UserInterface::new(
             paused,
             aborted,
-            state_rx,
+            shutdown.watch(),
             config.clone(),
             stdout.bytes_transferred(),
-            watch_tx,
+            config_tx,
         )?;
         Some(thread::spawn(|| ui.run(Instant::now())))
     } else {
         None
     };
     let copy_result = copy(&mut stdin.lock(), &mut stdout);
-    state_tx.set(ProgressMessage::Interrupted);
+    shutdown.on();
     if let Some(ui) = ui {
         match ui.join() {
             Err(_) | Ok(Err(_)) => {
