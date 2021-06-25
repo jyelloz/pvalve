a5c03cb54d7d7972f4a780c8d40f4fa0b16698bc
Jordan Yelloz <jordan@yelloz.me>
2021-06-25 07:32:05 -0700

    cli: Cleaned up a little.
    Removed the duplicated Unit type.

--- src/cli.rs
+++ src/cli.rs
@@ -1,52 +1,48 @@
+use std::num::NonZeroU32;
+
 use gumdrop::Options;
 
-use std::{
-    num::{NonZeroU32, ParseIntError},
-    str::FromStr,
-};
+use super::config::Unit;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialOrd, Ord, PartialEq, Eq)]
 pub struct Speed(pub NonZeroU32);
 
-impl FromStr for Speed {
-    type Err = ParseIntError;
+impl std::str::FromStr for Speed {
+    type Err = std::num::ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         NonZeroU32::from_str(s).map(|i| Self(i))
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum Mode {
-    Bytes,
-    Lines,
-    Nulls,
-}
-
-impl Default for Mode {
-    fn default() -> Self {
-        Self::Bytes
+impl Into<NonZeroU32> for &Speed {
+    fn into(self) -> NonZeroU32 {
+        self.0
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 pub struct Invocation {
     pub speed: Option<Speed>,
-    pub mode: Mode,
+    pub unit: Unit,
 }
 
-#[derive(Debug, Options)]
+#[derive(Debug, Default, Clone, Copy, Options)]
 pub struct Opts {
-    #[options(short = "L")]
+    #[options(
+        short = "L",
+        help = "the maximum number of items to allow through per second",
+    )]
     speed_limit: Option<Speed>,
     #[options(
-        help = "enable line-oriented mode where measurements apply to lines, not bytes"
+        short = "l",
+        help = "enable line-oriented mode where measurements apply to lines, not bytes",
     )]
     line_mode: bool,
     #[options(
         short = "0",
         long = "null",
-        help = "same as line-oriented mode, except the lines are NUL-separated"
+        help = "similar to line-oriented mode, except the lines are NUL-separated"
     )]
     null_mode: bool,
     help: bool,
@@ -55,27 +51,27 @@ pub struct Opts {
 impl Opts {
     pub fn parse_process_args() -> Invocation {
         let opts = Self::parse_args_default_or_exit();
-        Invocation::from(opts)
+        opts.into()
     }
 }
 
-impl From<&Opts> for Mode {
+impl From<&Opts> for Unit {
     fn from(opts: &Opts) -> Self {
         if opts.null_mode {
-            Self::Nulls
+            Self::Null
         } else if opts.line_mode {
-            Self::Lines
+            Self::Line
         } else {
-            Self::Bytes
+            Self::Byte
         }
     }
 }
 
 impl From<Opts> for Invocation {
     fn from(opts: Opts) -> Self {
-        let mode = Mode::from(&opts);
+        let unit = Unit::from(&opts);
         let speed = opts.speed_limit;
-        Invocation { mode, speed }
+        Self { unit, speed }
     }
 }
 
@@ -83,27 +79,44 @@ impl From<Opts> for Invocation {
 #[allow(non_snake_case)]
 mod tests {
     use super::*;
+    type Result = anyhow::Result<()>;
     #[test]
-    fn when__mode_not_selected__then__bytes_is_used() -> anyhow::Result<()> {
+    fn when__unit_not_selected__then__bytes_is_used() -> Result {
         let opts = Opts::parse_args_default::<&str>(&[])?;
-        let mode = Invocation::from(opts).mode;
-        assert_eq!(mode, Mode::Bytes,);
+        let unit = Invocation::from(opts).unit;
+        assert_eq!(unit, Unit::Byte);
         Ok(())
     }
 
     #[test]
-    fn when__line_mode_selected__then__lines_is_used() -> anyhow::Result<()> {
+    fn when__line_unit_selected__then__line_is_used() -> Result {
         let opts = Opts::parse_args_default(&["-l"])?;
-        let mode = Invocation::from(opts).mode;
-        assert_eq!(mode, Mode::Lines,);
+        let unit = Invocation::from(opts).unit;
+        assert_eq!(unit, Unit::Line);
         Ok(())
     }
 
     #[test]
-    fn when__null_mode_selected__then__nulls_is_used() -> anyhow::Result<()> {
+    fn when__null_unit_selected__then__null_is_used() -> Result {
         let opts = Opts::parse_args_default(&["-0"])?;
-        let mode = Invocation::from(opts).mode;
-        assert_eq!(mode, Mode::Nulls,);
+        let unit = Invocation::from(opts).unit;
+        assert_eq!(unit, Unit::Null);
+        Ok(())
+    }
+
+    #[test]
+    fn when__line_and_null_units_selected__then__null_is_used() -> Result {
+        let opts = Opts::parse_args_default(&["-l", "-0"])?;
+        let unit = Invocation::from(opts).unit;
+        assert_eq!(unit, Unit::Null);
+        Ok(())
+    }
+
+    #[test]
+    fn when__null_and_line_units_selected__then__null_is_used() -> Result {
+        let opts = Opts::parse_args_default(&["-0", "-l"])?;
+        let unit = Invocation::from(opts).unit;
+        assert_eq!(unit, Unit::Null);
         Ok(())
     }
 }
